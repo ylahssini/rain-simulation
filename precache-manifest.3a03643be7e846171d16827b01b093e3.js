2f421fe4c404164e6469c26b013474ee30aac737
ylahssini <ylahssini@gmail.com>
2020-09-27 20:21:30 +0100

    Updates

---
+++ precache-manifest.3a03643be7e846171d16827b01b093e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076c6669a3377a5caab93eeaa9611b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b46d57def86be16c6100",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0ce6a1b317af4013de68",
+    "url": "/static/js/2.50061533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50061533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46d57def86be16c6100",
+    "url": "/static/js/main.482b5f36.chunk.js"
+  },
+  {
+    "revision": "a4b35e34b37da9faf9d5",
+    "url": "/static/js/runtime-main.215fb8d8.js"
+  }
+]);
