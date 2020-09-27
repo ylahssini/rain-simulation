40b7d1edd8208792e097f358a730f36774f90415
ylahssini <ylahssini@gmail.com>
2020-09-27 20:10:16 +0100

    react-wrapper closed door

--- src/Rain/Particles.ts
+++ src/Rain/Particles.ts
@@ -1,4 +1,4 @@
-import p5Types from 'p5';
+import p5 from 'p5';
 
 interface DropInterface {
     fall: () => void;
@@ -8,37 +8,41 @@ interface DropInterface {
 export const drops: DropInterface[] = [];
 
 export class RainDrop {
-    p5: p5Types;
+    p: p5;
+    playSound: () => void;
     position: any;
     ySpeed: number;
     width: number;
     height: number;
 
-    constructor(p5: p5Types) {
-        this.p5 = p5;
+    constructor(p: p5, playSound: () => void) {
+        this.p = p;
+        this.playSound = playSound;
 
-        const y = this.p5.random(-500, -50);
-        this.position = this.p5.createVector(this.p5.random(this.p5.windowWidth), y);
-        this.ySpeed = this.p5.random(3, 9);
+        const y = this.p.random(-500, -50);
+        this.position = this.p.createVector(this.p.random(this.p.windowWidth), y);
+        this.ySpeed = this.p.random(3, 9);
 
-        this.width = this.p5.random(1, 1.5);
-        this.height = this.p5.random(10, 30);
+        this.width = this.p.random(1, 1.5);
+        this.height = this.p.random(10, 30);
     }
 
     fall() {
         this.position.y = this.position.y + this.ySpeed;
         this.ySpeed = this.ySpeed + 0.15;
 
-        if (this.position.y > this.p5.windowHeight) {
-            this.position.y = this.p5.random(-150, -200);
-            this.ySpeed = this.p5.random(3, 9);
+        if (this.position.y > this.p.windowHeight) {
+            this.position.y = this.p.random(-150, -200);
+            this.ySpeed = this.p.random(3, 9);
+
+            this.playSound();
 
             drops.splice(drops.indexOf(this), 1);
         }
     }
 
     show() {
-        this.p5.fill(0);
-        this.p5.rect(this.position.x, this.position.y, this.width, this.height);
+        this.p.fill(0);
+        this.p.rect(this.position.x, this.position.y, this.width, this.height);
     }
 }
--- src/Rain/index.tsx
+++ src/Rain/index.tsx
@@ -1,32 +1,46 @@
-import React from 'react';
-import Sketch from 'react-p5';
-import p5Types from 'p5';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useRef, useEffect } from 'react';
+import p5 from 'p5';
+import { preBloop, playBloop } from './Bloops';
 import { drops, RainDrop } from './Particles';
 
-const Rain = (): React.ReactElement => {
-    function setup(p5: p5Types, elementCanvas: Element): void {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(elementCanvas);
+const Rain = (): React.ReactElement | null => {
+    const p5Ref: React.MutableRefObject<HTMLDivElement | undefined> = useRef();
+
+    function sketch(p: p5) {
+        p.preload = (): void => {
+            preBloop(p);
+        };
+
+        p.setup = (): void => {
+            p.createCanvas(p.windowWidth, p.windowHeight);
+        }
+
+        p.draw = (): void => {
+            p.background(255);
+
+            for (let i = 0; i < p.random(44); i += 1) {
+                drops.push(new RainDrop(p, playBloop));
+            }
+
+            for (let i = 0; i < drops.length; i += 1) {
+                const drop = drops[i];
+                drop.fall();
+                drop.show();
+            }
+        }
     }
 
-    function draw(p5: p5Types): void {
-        p5.background(255);
+    useEffect(() => {
+        new p5(sketch, p5Ref.current)
+    }, []);
 
-        for (let i = 0; i < p5.random(44); i += 1) {
-            drops.push(new RainDrop(p5));
-        }
-
-        for (let i = 0; i < drops.length; i += 1) {
-            const drop = drops[i];
-            drop.fall();
-            drop.show();
-        }
+    if (!p5Ref) {
+        return null;
     }
 
     return (
-        <Sketch
-            setup={setup}
-            draw={draw}
-        />
+        <></>
     );
 }
 
